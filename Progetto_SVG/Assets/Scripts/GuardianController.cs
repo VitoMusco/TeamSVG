2abee0ad04badbd0009745e294fe739d65556a76
Vito Musco <v.musco1@studenti.uniba.it>
2022-05-24 18:10:37 +0200

    Stanza3 aggiunta

--- Progetto_SVG/Assets/Scripts/GuardianController.cs
+++ Progetto_SVG/Assets/Scripts/GuardianController.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardianController : MonoBehaviour
 {
+
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    
+    }
+
+    void Awake() {
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (playerInSightRange && !playerInAttackRange) Chase();
+        if (playerInAttackRange && playerInSightRange) Attack();
     }
+
+    private void Chase() {
+        agent.SetDestination(player.position);
+    }
+    private void Attack() {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked) {
+            //ATTACCA!
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack() {
+        alreadyAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
 }
