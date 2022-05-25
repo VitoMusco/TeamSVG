9a069720b93a05dccf4f6647b52966535f18d887
Vito Musco <v.musco1@studenti.uniba.it>
2022-05-25 16:29:09 +0200

    Aggiornamenti vari

--- Progetto_SVG/Assets/Scripts/GuardianController.cs
+++ Progetto_SVG/Assets/Scripts/GuardianController.cs
@@ -7,10 +7,14 @@ public class GuardianController : MonoBehaviour
 {
 
     public NavMeshAgent agent;
-
     public Transform player;
-
     public LayerMask whatIsGround, whatIsPlayer;
+
+    private Animator anim;
+
+    [SerializeField] private float action = 0f;
+    [SerializeField] private bool isWalking = false;
+    [SerializeField] private bool isAttacking = false;
 
     //Attacking
     public float timeBetweenAttacks;
@@ -28,6 +32,7 @@ public class GuardianController : MonoBehaviour
 
     void Awake() {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -38,19 +43,25 @@ public class GuardianController : MonoBehaviour
 
         if (playerInSightRange && !playerInAttackRange) Chase();
         if (playerInAttackRange && playerInSightRange) Attack();
+
+        handleAnimations();
     }
 
     private void Chase() {
         agent.SetDestination(player.position);
+        isWalking = true;
+        isAttacking = false;
     }
     private void Attack() {
         agent.SetDestination(transform.position);
-
-        transform.LookAt(player);
+        var playerPosition = player.position;
+        playerPosition.y = 0;
+        transform.LookAt(playerPosition);
 
         if (!alreadyAttacked) {
             //ATTACCA!
-
+            isWalking = false;
+            isAttacking = true;
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
@@ -60,11 +71,16 @@ public class GuardianController : MonoBehaviour
         alreadyAttacked = false;
     }
 
+    private void handleAnimations() {
+        action = isWalking ? Mathf.Lerp(action, 1f, 0.25f) : isAttacking ? Mathf.Lerp(action, 2f, 0.25f) : Mathf.Lerp(action, 0f, 0.25f);
+        anim.SetFloat("Blend", action);
+    }
+
+
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
     }
-
 }
