2d9dd0a4676aef004bd7981a6ec36f0e16ed8aac
Vito Musco <v.musco1@studenti.uniba.it>
2022-05-25 17:34:00 +0200

    Update GuardianController.cs

--- Progetto_SVG/Assets/Scripts/GuardianController.cs
+++ Progetto_SVG/Assets/Scripts/GuardianController.cs
@@ -14,15 +14,19 @@ public class GuardianController : MonoBehaviour
 
     [SerializeField] private float action = 0f;
     [SerializeField] private bool isWalking = false;
-    [SerializeField] private bool isAttacking = false;
+    [SerializeField] private float isAttacking = 0f;
+    [SerializeField] private float timeToSlam;
+    [SerializeField] private float timeToShoot;
+    [SerializeField] private bool isShooting = false;
+    [SerializeField] private bool isSlamming = false;
 
     //Attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
 
     //States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
+    public float sightRange, laserAttackRange, slamAttackRange;
+    public bool playerInSightRange, playerInLaserAttackRange, playerInSlamAttackRange;
 
     // Start is called before the first frame update
     void Start()
@@ -39,20 +43,25 @@ public class GuardianController : MonoBehaviour
     void Update()
     {
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        playerInLaserAttackRange = Physics.CheckSphere(transform.position, laserAttackRange, whatIsPlayer);
+        playerInSlamAttackRange = Physics.CheckSphere(transform.position, slamAttackRange, whatIsPlayer);
 
-        if (playerInSightRange && !playerInAttackRange) Chase();
-        if (playerInAttackRange && playerInSightRange) Attack();
+        if (playerInSightRange && !playerInLaserAttackRange) chase();
+        if (playerInSlamAttackRange && playerInSightRange) attack(2f);
+        else if (playerInLaserAttackRange && playerInSightRange) attack(3f);
 
         handleAnimations();
     }
 
-    private void Chase() {
+    private void chase() {
+        if (isAttacking != 0f) return;
+
         agent.SetDestination(player.position);
         isWalking = true;
-        isAttacking = false;
+        isAttacking = 0f;
     }
-    private void Attack() {
+    private void attack(float type) {
+
         agent.SetDestination(transform.position);
         var playerPosition = player.position;
         playerPosition.y = 0;
@@ -61,7 +70,7 @@ public class GuardianController : MonoBehaviour
         if (!alreadyAttacked) {
             //ATTACCA!
             isWalking = false;
-            isAttacking = true;
+            isAttacking = type;
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
@@ -72,14 +81,59 @@ public class GuardianController : MonoBehaviour
     }
 
     private void handleAnimations() {
-        action = isWalking ? Mathf.Lerp(action, 1f, 0.25f) : isAttacking ? Mathf.Lerp(action, 2f, 0.25f) : Mathf.Lerp(action, 0f, 0.25f);
-        anim.SetFloat("Blend", action);
+        if (!isWalking)
+            action = Mathf.Lerp(action, 0f, 0.25f);
+            anim.SetFloat("Blend", action);
+        if (isWalking) {
+            action = Mathf.Lerp(action, 1f, 0.25f);
+            anim.SetFloat("Blend", action);
+        }
+        else if(isAttacking != 0f) {
+            anim.SetFloat("Blend", isAttacking);
+            if (isAttacking == 2f && !isSlamming && !isShooting) {
+                StartCoroutine(slam());
+            }
+            if (isAttacking == 3f && !isSlamming && !isShooting)
+                StartCoroutine(shoot());
+        }
     }
 
+    IEnumerator slam() {
+        float timeElapsed = 0f;
+        anim.SetTrigger("Slam");
+        while (timeElapsed < timeToSlam) {
+            isSlamming = true;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        anim.ResetTrigger("Slam");
+        isSlamming = false;
+        isAttacking = 0f;
+    }
+
+    IEnumerator shoot() {
+        float timeElapsed = 0f;
+        anim.SetTrigger("StartShooting");
+        while (timeElapsed < timeToShoot)
+        {
+            isShooting = true;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        timeElapsed = 0f;
+        anim.ResetTrigger("StartShooting");
+        //Sparo
+        anim.SetTrigger("StopShooting");
+        anim.ResetTrigger("StartShooting");
+        isShooting = false;
+        isAttacking = 0f;
+    }
 
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.DrawWireSphere(transform.position, laserAttackRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, slamAttackRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
     }
