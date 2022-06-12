15b6975354fefe2f93fafb2abb1e8c6c864a0d82
Vito Musco <v.musco1@studenti.uniba.it>
2022-06-12 10:03:42 +0200

    Update PlayerMovement.cs

--- Progetto_SVG/Assets/Scripts/PlayerMovement.cs
+++ Progetto_SVG/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     public bool developerMode = false;
+    public bool hasRespawned = false;
     public Transform predictedMovement;
     public Transform shootSource;
     public Transform playerSpawnPosition;
@@ -50,7 +51,7 @@ public class PlayerMovement : MonoBehaviour
     private float x;
     private float z;
     private float lockX;
-    private float lockZ;
+    private float lockZ;  
 
     private Vector3 lockRight;
     private Vector3 lockForward;
@@ -65,8 +66,7 @@ public class PlayerMovement : MonoBehaviour
     private float crouchBobAmount = 0.025f;
     private float defaultYpos = 0;
     private float timer;
-    public bool isAlive = true;
-    public bool hasRespawned = false;
+    private bool isAlive = true;
 
     [SerializeField] private float health = 100f;
     [SerializeField] private float maxHealth = 100f;
@@ -94,10 +94,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private bool hasDoubleJumped = false;
     [SerializeField] private float maxTimeAfterAnAction = 2f;
 
-    void Awake()
-    {
-        if (developerMode)
-        {
+    void Awake() {
+        if (developerMode) {
             health = 1000000;
             magicStamina = 1000000;
         }
@@ -116,22 +114,18 @@ public class PlayerMovement : MonoBehaviour
         StartCoroutine(handleStamina());
     }
     // Update is called once per frame
-    void Update()
-    {
-        if (isAlive)
-        {
+    void Update() {
+        if (isAlive) {
             checkIfGrounded();
             handleInputs();
             handleFootSteps();
             handleMovementPrediction();
-            handleAnimations();
-        }
+            handleAnimations(); 
+        }     
     }
 
-    void LateUpdate()
-    {
-        if (!isAlive && !hasRespawned)
-        {
+    void LateUpdate() {
+        if (!isAlive && !hasRespawned) {
             isAlive = true;
             hasRespawned = true;
         }
@@ -143,8 +137,7 @@ public class PlayerMovement : MonoBehaviour
     //CONTROLLA SE SI E' A TERRA
     void checkIfGrounded() => isGrounded = controller.isGrounded;
 
-    void checkIfMoving(float x, float z)
-    {
+    void checkIfMoving(float x, float z) {
         if (Mathf.Abs(x) > 0.1 || Mathf.Abs(z) > 0.1)
             isMoving = true;
         else
@@ -152,7 +145,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (isMoving && speed == 3f && !isCrouching && isGrounded)
             isWalking = true;
-        else
+        else 
             isWalking = false;
 
         if (isMoving && speed == 6f && !isCrouching && isGrounded)
@@ -162,21 +155,18 @@ public class PlayerMovement : MonoBehaviour
     }
 
     //GESTIONE INPUT
-    void handleInputs()
-    {
+    void handleInputs() {
         //MOVIMENTO
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
 
         verticalVelocity = velocity.y;
 
-        if (isGrounded)
-        {
+        if (isGrounded) {
             velocity = transform.right * x + transform.forward * z;
             velocity = velocity * speed;
         }
-        else
-        {
+        else {
             velocity = lockRight * lockX + lockForward * lockZ;
             velocity = velocity * lockSpeed;
         }
@@ -233,20 +223,17 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //GESTIONE SALTO E DOPPIO SALTO
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
+        if (Input.GetButtonDown("Jump") && isGrounded) {
             jump();
             canDoubleJump = true;
         }
-        else if (Input.GetButtonDown("Jump") && canDoubleJump)
-        {
+        else if (Input.GetButtonDown("Jump") && canDoubleJump) {
             StartCoroutine(handleDoubleJump());
             hasDoubleJumped = true;
             jump();
             canDoubleJump = false;
         }
-        if (hasDoubleJumped && isGrounded)
-        {
+        if (hasDoubleJumped && isGrounded) {
             hasDoubleJumped = false;
         }
 
@@ -254,8 +241,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && canShoot && !isDefending && magicStamina > 0f)
         {
             //ATTACCA
-            if (canShoot)
-            {
+            if (canShoot) {
                 isAttacking = true;
                 Invoke(nameof(shoot), timeToShoot);
                 canShoot = false;
@@ -264,15 +250,13 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //GESTIONE DIFESA
-        if (Input.GetMouseButtonDown(1) && !isAttacking && magicStamina > 0f)
-        {
+        if (Input.GetMouseButtonDown(1) && !isAttacking && magicStamina > 0f) {
             shieldRenderer.enabled = true;
             decalRenderer.enabled = true;
             isDefending = true;
             shieldSoundSource.Play();
         }
-        if (Input.GetMouseButtonUp(1) || magicStamina == 0f)
-        {
+        if (Input.GetMouseButtonUp(1) || magicStamina == 0f) {
             shieldSoundSource.Stop();
             shieldRenderer.enabled = false;
             decalRenderer.enabled = false;
@@ -295,33 +279,27 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void handleMovementPrediction()
-    {
+    void handleMovementPrediction() {
         Vector3 XZVelocity = velocity;
         XZVelocity.y = 0;
         predictedMovement.position = Vector3.Lerp(predictedMovement.position, transform.position + XZVelocity * predictedVelocityMultiplier, 0.25f);
     }
 
-    void resetShoot()
-    {
+    void resetShoot() {
         canShoot = true;
     }
 
-    void handleFallDamage()
-    {
-        if (isGrounded && velocity.y < -30)
-        {
+    void handleFallDamage() {
+        if (isGrounded && velocity.y < -30) {
             health = 0;
             checkHealth();
         }
     }
 
-    void handleHeadBob()
-    {
+    void handleHeadBob() {
         if (!isGrounded) return;
 
-        if (isMoving)
-        {
+        if (isMoving) {
             timer += Time.deltaTime * (isCrouching ? crouchBobSpeed : isWalking ? walkBobSpeed : runBobSpeed);
             playerCamera.transform.localPosition = new Vector3(
                 playerCamera.transform.localPosition.x,
@@ -330,8 +308,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    IEnumerator handleCrouch()
-    {
+    IEnumerator handleCrouch() {
 
         bool applyGravity = false;
         float currentHeight = controller.height;
@@ -341,7 +318,7 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 currentCenter = controller.center;
         Vector3 targetCenter = isCrouching ? new Vector3(0f, 0f, 0f) : new Vector3(0f, 0.25f, 0f);
-
+        
         canCrouch = false;
 
         if (isRunning)
@@ -349,8 +326,7 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded)
             applyGravity = true;
 
-        while (timeElapsed < timeToCrouch)
-        {
+        while (timeElapsed < timeToCrouch) {
             controller.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed / timeToCrouch);
             controller.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed / timeToCrouch);
             timeElapsed += Time.deltaTime;
@@ -368,8 +344,7 @@ public class PlayerMovement : MonoBehaviour
         canCrouch = true;
     }
 
-    void shoot()
-    {
+    void shoot() {
         RaycastHit hit;
         shootBeam.enabled = true;
         shootBeam.SetPosition(0, shootSource.transform.position);
@@ -381,8 +356,7 @@ public class PlayerMovement : MonoBehaviour
             if (hit.collider.tag == "Guardian")
                 hit.collider.GetComponent<GuardianController>().takeDamage(attackDamage);
         }
-        else
-        {
+        else {
             shootBeam.SetPosition(1, playerCamera.transform.position + playerCamera.transform.forward * 100f);
         }
         particleShoot.transform.position = shootSource.transform.position;
@@ -393,12 +367,10 @@ public class PlayerMovement : MonoBehaviour
         //Invoke(nameof(removeBeam), 1f);
     }
 
-    IEnumerator expandShootBeam()
-    {
+    IEnumerator expandShootBeam() {
         float timeToExpand = 0.25f;
         float timeElapsed = 0f;
-        while (timeElapsed < timeToExpand)
-        {
+        while (timeElapsed < timeToExpand) {
             timeElapsed += Time.deltaTime;
             shootBeam.widthMultiplier = Mathf.Lerp(shootBeam.widthMultiplier, 2f, timeElapsed / timeToExpand);
             yield return null;
@@ -406,8 +378,7 @@ public class PlayerMovement : MonoBehaviour
         shootBeam.widthMultiplier = 1f;
     }
 
-    IEnumerator shrinkShootBeam()
-    {
+    IEnumerator shrinkShootBeam() {
         float timeToShrink = 0.25f;
         float timeElapsed = 0f;
         while (timeElapsed < timeToShrink)
@@ -420,10 +391,8 @@ public class PlayerMovement : MonoBehaviour
         shootBeam.enabled = false;
     }
 
-    void handleAnimations()
-    {
-        if (isAttacking)
-        {
+    void handleAnimations() {
+        if (isAttacking) {
             anim.SetInteger("Attacking", Random.Range(1, 3));
         }
         else
@@ -433,9 +402,9 @@ public class PlayerMovement : MonoBehaviour
             anim.SetBool("Defending", true);
         else
             anim.SetBool("Defending", false);
-
+        
         if (isGrounded)
-            action = isCrouching ? Mathf.Lerp(action, 2, 0.25f) : isWalking ? Mathf.Lerp(action, 4f, 0.25f) :
+            action = isCrouching ? Mathf.Lerp(action, 2, 0.25f) : isWalking ? Mathf.Lerp(action, 4f, 0.25f) : 
                 isRunning ? Mathf.Lerp(action, 5f, 0.25f) : Mathf.Lerp(action, 3f, 0.1f);
         else
             action = isLevitating ? Mathf.Lerp(action, 1, 0.25f) : Mathf.Lerp(action, 6f, 0.1f);
@@ -443,10 +412,9 @@ public class PlayerMovement : MonoBehaviour
         anim.SetFloat("Blend", action);
     }
 
-    void jump()
-    {
+    void jump() {
         if (isCrouching && Physics.Raycast(playerCamera.transform.position, Vector3.up, 1f)) return;
-        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);       
         lockRight = transform.right;
         lockForward = transform.forward;
         lockX = x;
@@ -454,13 +422,11 @@ public class PlayerMovement : MonoBehaviour
         lockSpeed = speed;
     }
 
-    IEnumerator handleDoubleJump()
-    {
+    IEnumerator handleDoubleJump() {
         float timeToFinish = 0.36f;
         float timeElapsed = 0;
         anim.SetBool("DoubleJumping", true);
-        while (timeElapsed < timeToFinish)
-        {
+        while (timeElapsed < timeToFinish) {
             timeElapsed += Time.deltaTime;
 
             yield return null;
@@ -469,13 +435,11 @@ public class PlayerMovement : MonoBehaviour
     }
     void checkHealth()
     {
-        if (health <= 0)
-        {
+        if (health <= 0) {
             isAlive = false;
             die();
         }
-        else
-        {
+        else {
             print("Salute rimanente: " + health);
         }
     }
@@ -484,8 +448,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isAlive)
         {
-            if (!isDefending)
-            {
+            if (!isDefending) {
                 print("Ho preso " + damageAmount + " danni");
                 health -= damageAmount;
                 healthBar.fillAmount = health / maxHealth;
@@ -498,8 +461,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void die()
-    {
+    void die() {
         transform.localPosition = playerSpawnPosition.localPosition;
         transform.localRotation = playerSpawnPosition.localRotation;
         health = maxHealth;
@@ -509,8 +471,7 @@ public class PlayerMovement : MonoBehaviour
         print("sono morto!");
     }
 
-    IEnumerator handleStamina()
-    {
+    IEnumerator handleStamina() {
         float staminaToAdd = 5f;
         float staminaRemovalTime = 0.25f;
         float staminaAddTime = 0.25f;
@@ -518,57 +479,51 @@ public class PlayerMovement : MonoBehaviour
         float attackTime = 0f;
         float defenseTime = 0f;
         float runTime = 0f;
-
-        while (isAlive)
-        {
+        
+        while (isAlive) {           
             if (timeAfterAnAction < maxTimeAfterAnAction)
                 timeAfterAnAction += Time.deltaTime;
-            if (attackTime < maxTimeAfterAnAction)
+            if(attackTime < maxTimeAfterAnAction)
                 attackTime += Time.deltaTime;
-            if (defenseTime < maxTimeAfterAnAction)
+            if(defenseTime < maxTimeAfterAnAction)
                 defenseTime += Time.deltaTime;
-            if (runTime < maxTimeAfterAnAction)
+            if(runTime < maxTimeAfterAnAction)
                 runTime += Time.deltaTime;
-            if (stopTime < maxTimeAfterAnAction)
+            if(stopTime < maxTimeAfterAnAction)
                 stopTime += Time.deltaTime;
 
-            if (isAttacking && attackTime > staminaRemovalTime)
-            {
+            if (isAttacking && attackTime > staminaRemovalTime) {
                 staminaToRemove = attackStaminaRemove;
                 attackTime = 0f;
             }
-
-            if (isDefending && defenseTime > staminaAddTime)
-            {
-                if (magicStamina + defenseStaminaAdd <= maxMagicStamina)
+                
+            if (isDefending && defenseTime > staminaAddTime) {
+                if(magicStamina + defenseStaminaAdd <= maxMagicStamina)
                     magicStamina += defenseStaminaAdd;
                 defenseTime = 0f;
             }
-
-            if (isRunning && runTime > staminaRemovalTime)
-            {
+                
+            if (isRunning && runTime > staminaRemovalTime) {
                 staminaToRemove = runStaminaRemove;
                 runTime = 0f;
             }
             if (staminaToRemove > 0f)
                 timeAfterAnAction = 0f;
-            if (timeAfterAnAction >= maxTimeAfterAnAction && stopTime > staminaAddTime && magicStamina < maxMagicStamina)
-            {
+            if (timeAfterAnAction >= maxTimeAfterAnAction && stopTime > staminaAddTime && magicStamina < maxMagicStamina) {
                 if (magicStamina + staminaToAdd > maxMagicStamina)
                     magicStamina = maxMagicStamina;
                 else
                     magicStamina += staminaToAdd;
                 stopTime = 0f;
             }
-            else
-            {
+            else {
                 if (magicStamina - staminaToRemove < 0f)
                     magicStamina = 0f;
                 else
                     magicStamina -= staminaToRemove;
             }
             staminaBar.fillAmount = magicStamina / maxMagicStamina;
-            staminaBarEnd.transform.localPosition = new Vector2(staminaBarEndStartPosition - (250 - (250 / maxMagicStamina) * magicStamina), staminaBarEnd.transform.localPosition.y);
+            staminaBarEnd.transform.localPosition = new Vector2(staminaBarEndStartPosition - (250 - (250/maxMagicStamina) * magicStamina), staminaBarEnd.transform.localPosition.y);
             staminaToRemove = 0f;
             yield return null;
         }
