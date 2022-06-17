569678527e29b3455111f636b15c63fe46482a40
Vito Musco <v.musco1@studenti.uniba.it>
2022-06-17 14:29:36 +0200

    Aggiornato labirinto

--- Progetto_SVG/Assets/Scripts/MazeScripts/DoorUnlockerBehaviour.cs
+++ Progetto_SVG/Assets/Scripts/MazeScripts/DoorUnlockerBehaviour.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class DoorUnlockerBehaviour : MonoBehaviour
 {
     public QuizBoxBehaviour[] quizBoxes;
+
+    public Transform door;
+    public Transform doorEndTransform;
+
+    private Vector3 doorStartPosition;
     private bool doorUnlocked = false;
     private bool isCombinationCorrect;
+
+
+    void Awake() {
+        doorStartPosition = door.position;
+    }
+
     void Update() {
         if(!doorUnlocked) checkIfCombinationCorrect();
     }
@@ -17,12 +28,21 @@ public class DoorUnlockerBehaviour : MonoBehaviour
             isCombinationCorrect = true;
             if (!quizBox.getCorrectAnswer()) isCombinationCorrect = false;
         }
-        if (isCombinationCorrect) unlockDoor();
+        if (isCombinationCorrect) StartCoroutine(unlockDoor());
         else isCombinationCorrect = false;
     }
 
-    void unlockDoor() {
-        Debug.Log("DOOR UNLOCKED!");
+    IEnumerator unlockDoor() {
+        float timeElapsed = 0f;
+        float timeToMove = 5f;
         doorUnlocked = true;
+
+        while (timeElapsed < timeToMove) {
+            timeElapsed += Time.deltaTime;
+            door.position = Vector3.Lerp(doorStartPosition, doorEndTransform.position, timeElapsed / timeToMove);
+
+            yield return null;
+        }
+        door.position = doorEndTransform.position;
     }
 }
