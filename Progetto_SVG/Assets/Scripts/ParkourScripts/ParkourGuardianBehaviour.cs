7b3f7662042841577c3075b7b031b2cd27bf04ee
Vito Musco <v.musco1@studenti.uniba.it>
2022-07-04 18:36:11 +0200

    Aggiornamenti vari

---
+++ Progetto_SVG/Assets/Scripts/ParkourScripts/ParkourGuardianBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkourGuardianBehaviour : MonoBehaviour
+{
+    public Transform player;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+    }
+}
