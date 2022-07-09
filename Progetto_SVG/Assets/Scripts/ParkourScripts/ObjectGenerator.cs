e7ad5b0cd6d3dd3fccbfce2ec52427228a02d6c0
DPazienza <79839347+DPazienza@users.noreply.github.com>
2022-07-09 11:49:39 +0200

    DFF

--- Progetto_SVG/Assets/Scripts/ParkourScripts/ObjectGenerator.cs
+++ Progetto_SVG/Assets/Scripts/ParkourScripts/ObjectGenerator.cs
@@ -6,19 +6,23 @@ public class ObjectGenerator : MonoBehaviour
 {
     //[SerializeField] bool Xaxis = true;
     [SerializeField] float spawnTime;
+    [SerializeField] float waitTime;
     private float timeToSpawn = 0;
     public GameObject dmgObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeToSpawn -= waitTime;
+        dmgObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         timeToSpawn += Time.deltaTime;
+        
+        
         if(timeToSpawn >= spawnTime)
         {
 
